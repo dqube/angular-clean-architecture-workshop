888c85e8fbfcb24dccf3eb29f924544b8b596213
Matt Vaughn <matt.vaughn@buildmotion.com>
2021-11-04 18:53:18 -0600

    minor content changes;

--- workspace/apps/black-dashboard/src/app/app.module.ts
+++ workspace/apps/black-dashboard/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { ComponentsModule } from './components/components.module';
+
 
 @NgModule({
   declarations: [
