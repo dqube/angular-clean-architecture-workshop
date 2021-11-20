5e0a92c2f294411811bc226fc49b3917cb8f93ce
Matt Vaughn <matt.vaughn@buildmotion.com>
2021-11-20 13:10:20 -0700

    add validators

--- workspace/libs/accounts/accounts-ui/src/lib/new-account/new-account.component.ts
+++ workspace/libs/accounts/accounts-ui/src/lib/new-account/new-account.component.ts
@@ -4,11 +4,13 @@ import { Router } from '@angular/router';
 import { ComponentBase } from '@buildmotion/foundation';
 import { LoggingService, Severity } from '@buildmotion/logging';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { ValidationService } from '@buildmotion/validation';
 
 @Component({
   selector: 'buildmotion-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [ValidationService]
 })
 export class NewAccountComponent extends ComponentBase implements OnInit {
 
@@ -17,9 +19,26 @@ export class NewAccountComponent extends ComponentBase implements OnInit {
   // shared password validators;
   private passwordValidators = [Validators.required, Validators.minLength(8), Validators.maxLength(128)];
 
+  // inputs for the [PasswordStrength] indicator
+  hasEightChars: boolean;
+  hasLowercase: boolean;
+  hasNumber: boolean;
+  hasSpecialChars: boolean;
+  hasUppercase: boolean;
+  isMedium: boolean;
+  isStrong: boolean;
+  isWeak: boolean;
+  withoutEightChars: boolean;
+  withoutLowercase: boolean;
+  withoutNumber: boolean;
+  withoutSpecialChar: boolean;
+  withoutUppercase: boolean;
+  strength: number;
+
 
   constructor(
     private formBuilder: FormBuilder,
+    private validationService: ValidationService,
     loggingService: LoggingService,
     router: Router
   ) {
@@ -53,6 +72,9 @@ export class NewAccountComponent extends ComponentBase implements OnInit {
     this.onPasswordValueChange();
   }
 
+  /**
+   * Use to coordinate changes in the password and password confirm inputs.
+   */
   private onPasswordValueChange() {
     this.passwordConfirm.valueChanges.pipe(debounceTime(1), distinctUntilChanged()).subscribe(() => {
       this.passwordConfirm.setErrors(null);
@@ -67,13 +89,108 @@ export class NewAccountComponent extends ComponentBase implements OnInit {
     });
   }
 
-  private determinePasswordStrength(value: any) {
-    throw new Error('Method not implemented.');
+  /**
+   * Use to determine the strength of the password based on the password requirements.
+   * @param password
+   */
+  private determinePasswordStrength(password: string) {
+    this.strength = [
+      this.validatePasswordLowercase(password),
+      this.validatePasswordMinLength(password),
+      this.validatePasswordNumeric(password),
+      this.validatePasswordSpecialCharacter(password),
+      this.validatePasswordUppercase(password),
+    ].filter((item) => item !== false).length;
+
+    this.loggingService.log(this.componentName, Severity.Information, `Preparing to set password strength: ${this.strength}`);
+
+    this.isWeak = false;
+    this.isMedium = false;
+    this.isStrong = false;
+
+    // update the progress/meter to indicate strength
+    if (this.strength === 5) {
+      this.isWeak = true;
+      this.isMedium = true;
+      this.isStrong = true;
+    }
+    if (this.strength >= 3) {
+      this.isWeak = true;
+      this.isMedium = true;
+    }
+    if (this.strength >= 1 && this.strength < 3) {
+      this.isWeak = true;
+    }
+    if (this.strength < 1) {
+      this.isWeak = false;
+      this.isMedium = false;
+      this.isStrong = false;
+    }
   }
 
   private resetStrengthIndicators() {
-    throw new Error('Method not implemented.');
+    this.hasEightChars = false;
+    this.hasLowercase = false;
+    this.hasNumber = false;
+    this.hasSpecialChars = false;
+    this.hasUppercase = false;
+
+    this.isMedium = false;
+    this.isStrong = false;
+    this.isWeak = false;
+
+    this.withoutEightChars = false;
+    this.withoutLowercase = false;
+    this.withoutNumber = false;
+    this.withoutSpecialChar = false;
+    this.withoutUppercase = false;
   }
+
+  private validatePasswordLowercase(value: string): boolean {
+    const isValid = this.validationService.hasLowercase(value);
+    this.hasLowercase = isValid;
+    if (this.withoutLowercase && isValid) {
+      this.withoutLowercase = false;
+    }
+    return isValid;
+  }
+
+  private validatePasswordMinLength(value: string): boolean {
+    const isValid = this.validationService.StringMinLengthIsValid(value, 8);
+    this.hasEightChars = isValid;
+    if (this.withoutEightChars && isValid) {
+      this.withoutEightChars = false;
+    }
+    return isValid;
+  }
+
+  private validatePasswordNumeric(value: string): boolean {
+    const isValid = this.validationService.hasNumber(value);
+    this.hasNumber = isValid;
+    if (this.withoutNumber && isValid) {
+      this.withoutNumber = false;
+    }
+    return isValid;
+  }
+
+  private validatePasswordSpecialCharacter(value: string): boolean {
+    const isValid = this.validationService.hasSpecialCharacters(value);
+    this.hasSpecialChars = isValid;
+    if (this.withoutSpecialChar && isValid) {
+      this.withoutSpecialChar = false;
+    }
+    return isValid;
+  }
+
+  private validatePasswordUppercase(value: string): boolean {
+    const isValid = this.validationService.hasUppercase(value);
+    this.hasUppercase = isValid;
+    if (this.withoutUppercase && isValid) {
+      this.withoutUppercase = false;
+    }
+    return isValid;
+  }
+
 
   get emailAddress(): AbstractControl {
     return this.form.get('emailAddress') as FormControl;
