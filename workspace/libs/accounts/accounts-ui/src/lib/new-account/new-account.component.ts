868b7e2d796617eac7b0ade655beed0553b696d9
Matt Vaughn <matt.vaughn@buildmotion.com>
2021-11-20 12:28:25 -0700

    add account components; setup new account ctr

--- workspace/libs/accounts/accounts-ui/src/lib/new-account/new-account.component.ts
+++ workspace/libs/accounts/accounts-ui/src/lib/new-account/new-account.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ComponentBase } from '@buildmotion/foundation';
+import { LoggingService, Severity } from '@buildmotion/logging';
 
 @Component({
   selector: 'buildmotion-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css']
 })
-export class NewAccountComponent implements OnInit {
+export class NewAccountComponent extends ComponentBase implements OnInit {
 
-  constructor() { }
+  constructor(
+    loggingService: LoggingService,
+    router: Router
+  ) {
+    super('JobInformationComponent', loggingService, router);
+  }
 
   ngOnInit(): void {
+    this.loggingService.log(this.componentName, Severity.Information, `Preparing to initialize component.`);
   }
 
 }
