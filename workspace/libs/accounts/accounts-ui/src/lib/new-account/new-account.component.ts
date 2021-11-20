58ef0825d5160af9ff7ba9d2721a71af5156bbaa
Matt Vaughn <matt.vaughn@buildmotion.com>
2021-11-20 12:41:19 -0700

    initialize form; create form group

--- workspace/libs/accounts/accounts-ui/src/lib/new-account/new-account.component.ts
+++ workspace/libs/accounts/accounts-ui/src/lib/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ComponentBase } from '@buildmotion/foundation';
 import { LoggingService, Severity } from '@buildmotion/logging';
@@ -10,15 +11,50 @@ import { LoggingService, Severity } from '@buildmotion/logging';
 })
 export class NewAccountComponent extends ComponentBase implements OnInit {
 
+  form: FormGroup;
+
+  // shared password validators;
+  private passwordValidators = [Validators.required, Validators.minLength(8), Validators.maxLength(128)];
+
+
   constructor(
+    private formBuilder: FormBuilder,
     loggingService: LoggingService,
     router: Router
   ) {
-    super('JobInformationComponent', loggingService, router);
+    super('NewAccountComponent', loggingService, router);
   }
 
   ngOnInit(): void {
     this.loggingService.log(this.componentName, Severity.Information, `Preparing to initialize component.`);
+    this.initializeForm();
   }
+
+  initializeForm() {
+    this.loggingService.log(this.componentName, Severity.Information, `Preparing to initialize the create account form.`);
+    this.form = this.formBuilder.group({
+      emailAddress: new FormControl(undefined, {
+        validators: [Validators.required, Validators.maxLength(100)],
+        asyncValidators: [
+          // ADD ASYNC EMAIL VALIDATION HERE;
+        ],
+        updateOn: 'blur',
+      }),
+      password: new FormControl(undefined, {
+        validators: [...this.passwordValidators],
+        asyncValidators: [],
+      }),
+      passwordConfirm: new FormControl(undefined, {
+        updateOn: 'change',
+      }),
+    });
+
+    this.onPasswordValueChange();
+  }
+
+  onPasswordValueChange() {
+    throw new Error('Method not implemented.');
+  }
+
 
 }
