b130f0de44ebda3265d220596ce60e83605b2306
Matt Vaughn <matt.vaughn@buildmotion.com>
2021-11-25 10:38:03 -0700

    add debug lanch configs

--- workspace/apps/accounts-api/src/accounts/accounts.service.ts
+++ workspace/apps/accounts-api/src/accounts/accounts.service.ts
@@ -1,11 +1,19 @@
 import { NewAccount, NewAccountResponse } from '@buildmotion/accounts/types';
 import { Injectable } from '@nestjs/common';
+import { Guid } from 'guid-typescript';
 
 @Injectable()
 export class AccountsService {
   createAccount(account: NewAccount): NewAccountResponse {
+    // FIXME: NEED REAL VALIDATION AND BUSINESS RULES --> SIMULATES RULE EVALUATION
     if (account && account.acceptTermsConditions) {
-      return new NewAccountResponse();
+      // FIXME: UPDATE TO USE REAL DATABASE --> SIMULATES DATA ACCESS;
+      const result: NewAccountResponse = new NewAccountResponse();
+      result.accountId = '1234';
+      result.emailAddress = account.emailAddress;
+      result.userId = Guid.create().toString();
+
+      return result;
     } else {
       throw new Error(`Failed to create account.`);
     }
